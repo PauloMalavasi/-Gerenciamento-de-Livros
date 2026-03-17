68a04455903cdd70cde2369357abd969082d18f8
unknown <paulomalavasi44@gmail.com>
2026-03-17 20:04:49 -0300

    Entity

---
+++ Livros/src/main/java/com/gerenciamento/Livros/Entity/LivroEntity.java
@@ -0,0 +1,54 @@
+package com.gerenciamento.Livros.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class LivroEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    @Column(name = "autorLivro")
+    private String autor;
+    @Column(name = "anoLancamento")
+    private Integer anoPublicacao;
+
+    public LivroEntity(Long id, String titulo, String autor, Integer anoPublicacao) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Integer getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(Integer anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+}
